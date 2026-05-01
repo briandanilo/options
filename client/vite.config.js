564d16d82fabe10b52ba6472231ba230c50bfaad
briandanilo <brian.danilo@gmail.com>
2026-05-01 16:46:51 -0700

    Bump to v1.04

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   define: {
     __BUILD_TIME__: JSON.stringify(new Date().toISOString()),
-    __VERSION__: JSON.stringify('v1.03'),
+    __VERSION__: JSON.stringify('v1.04'),
   },
   server: {
     proxy: {
